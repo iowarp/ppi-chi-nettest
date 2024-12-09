a6957d0f874fb903a3feeb02a0c46d30617710b9
lukemartinlogan <lukemartinlogan@gmail.com>
2024-12-09 08:22:09 -0600

    dont run daemon on first node

--- include/test_networks/rpc_thallium.h
+++ include/test_networks/rpc_thallium.h
@@ -108,7 +108,7 @@ class ThalliumRpc {
     for (thallium::async_response &resp : responses) {
       Wait<int>(resp);
     }
-    StopThisDaemon();
+    exit(1);
   }
 
   /** Thallium-compatible server name */
